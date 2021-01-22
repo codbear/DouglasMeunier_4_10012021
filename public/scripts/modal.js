7d970147e793af1101061a4824ea3054d591fa56
codbear <douglas.meunier@openclassrooms.com>
2021-01-22 14:43:55 +0100

    docs(modal): add JSDoc and comments

--- public/scripts/modal.js
+++ public/scripts/modal.js
@@ -1,3 +1,4 @@
+// DOM elements
 const $modal = document.querySelector('.bground');
 const $modalTriggers = document.querySelectorAll('.modal-btn');
 const $form = document.getElementById('signUpForm');
@@ -8,9 +9,9 @@ const $birthdate = document.getElementById('birthdate');
 const $quantity = document.getElementById('quantity');
 const $locations = document.querySelectorAll('input[type=radio]');
 const $tcu = document.getElementById('checkbox1');
-
 const modal = new Modal($modal);
 
+// Form instantiation
 const form = new Form({
   firstName: new Field([$first], isLongEnough),
   lastName: new Field([$last], isLongEnough),
@@ -21,10 +22,17 @@ const form = new Form({
   tcu: new Field([$tcu], isChecked),
 });
 
+// Open modal event
 $modalTriggers.forEach((btn) => btn.addEventListener('click', modal.open));
+
+// Form submit event
 $form.addEventListener('submit', validate);
 
-
+/**
+ * Triggers form validation and display success message if form is valid
+ *
+ * @param {Event} event
+ */
 function validate(event) {
   event.preventDefault();
 
